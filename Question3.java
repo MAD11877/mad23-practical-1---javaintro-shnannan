658b5f5499c020aa8d4160848480a7ae2e42fd9e
shnannan <116061929+shnannan@users.noreply.github.com>
2023-04-22 16:35:01 +0800

    My Answer for Question 3

--- Question3.java
+++ Question3.java
@@ -10,7 +10,14 @@ public class Question3
      * Hint: in.nextInt() to read integer
      */
      
-    Scanner in = new Scanner(System.in);
+    Scanner in = new Scanner(System.in);    // Creates Scanner Object 
     
+    System.out.println("Input an integer: ");
+    
+    int newInt = Integer * Integer; 
+    
+    System.out.println("New multiplied integer: " + newInt);    // Print multiplied integer 
+    
+    in.close();
   }
 }
