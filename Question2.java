76845a24ff5efb5dc2c8a6aceb670d271770c79a
shnannan <116061929+shnannan@users.noreply.github.com>
2023-04-22 14:02:48 +0800

    My Answer for Question 2
    calculating user's BMI with user input for height and weight.

--- Question2.java
+++ Question2.java
@@ -9,8 +9,21 @@ public class Question2
      * 
      * Hint: in.nextDouble() to read double
      */
-     
-    Scanner in = new Scanner(System.in);
-    double height = in.nextDouble();
+    
+    Scanner in = new Scanner(System.in);           // Creates Scanner object 
+    
+    System.out.println("Input height (in m): ");
+    
+    double height = in.nextDouble();              // Read user height input
+    
+    System.out.println("Input weight (in kg): ");  
+    
+    double weight = in.nextDouble();             // Read user weight input
+    
+    double BMI  = weight / (height * height);   // Calculate user BMI 
+    
+    System.out.print("Your Body Mass Index (BMI) is: " + BMI);  
+    
+    input.close(); 
   }
 }
